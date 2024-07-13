b262973cf1d3e79a6c51587312d27b80337fd83c
Holices Junior <dev.holices@gmail.com>
2024-07-13 22:00:23 +0100

    build: date format

--- src/pages/trip-details/activity-list.tsx
+++ src/pages/trip-details/activity-list.tsx
@@ -1,174 +1,58 @@
-import { CircleCheck, CircleDashed } from "lucide-react";
+import { CircleCheck } from "lucide-react";
+import { api } from "../../lib/axios";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { format } from "date-fns";
+import { ptBR } from "date-fns/locale";
 
-export function ActivityList() {
+interface Activity {
+  date: string;
+  activities: {
+    id: string;
+    title: string;
+    occurs_at: string;
+  }[]
+}
+
+export function Activities() {
+  const { tripId } = useParams()
+  const [activities, setActivities] = useState<Activity[]>([])
+
+  useEffect(() => {
+    api.get(`trips/${tripId}/activities`).then(response => setActivities(response.data.activities))
+  }, [tripId])
+
   return (
     <div className="space-y-8">
-      <div className="space-y-3">
-        <div className="flex gap-2 items-baseline">
-          <span className="text-xl text-zinc-300 font-semibold">Dia 17</span>
-          <span className="text-xs text-zinc-500">Sábado</span>
-        </div>
-        <p className="text-zinc-500 text-sm">Nenhuma atividade cadastrada nessa data.</p>
-      </div>
-
-      <div className="space-y-3">
-        <div className="flex gap-2 items-baseline">
-          <span className="text-xl text-zinc-300 font-semibold">Dia 18</span>
-          <span className="text-xs text-zinc-500">Domingo</span>
-        </div>
-        <div className="flex items-center px-4 py-2.5 bg-zinc-900 shadow-shape rounded-xl justify-between">
-          <div className="flex gap-3 items-center">
-            <CircleCheck className="size-5 text-lime-300" />
-            <p className="text-zinc-100 text-md">Corrida de Kart</p>
+      {activities.map(category => {
+        return (
+          <div key={category.date} className="space-y-2.5">
+            <div className="flex gap-2 items-baseline">
+              <span className="text-xl text-zinc-300 font-semibold">Dia {format(category.date, 'd')}</span>
+              <span className="text-xs text-zinc-500">{format(category.date, 'EEEE', { locale: ptBR })}</span>
+            </div>
+            {category.activities.length > 0 ? (
+              <div>
+                {category.activities.map(activity => {
+                  return (
+                    <div key={activity.id} className="space-y-2.5">
+                      <div className="px-4 py-2.5 bg-zinc-900 rounded-xl shadow-shape flex items-center gap-3">
+                        <CircleCheck className="size-5 text-lime-300" />
+                        <span className="text-zinc-100">{activity.title}</span>
+                        <span className="text-zinc-400 text-sm ml-auto">
+                          {format(activity.occurs_at, 'HH:mm')}h
+                        </span>
+                      </div>
+                    </div>
+                  )
+                })}
+              </div>
+            ) : (
+              <p className="text-zinc-500 text-sm">Nenhuma atividade cadastrada nessa data.</p>
+            )}
           </div>
-          <div>
-            <span className="text-sm text-zinc-400">14:00h</span>
-          </div>
-        </div>
-      </div>
-
-      <div className="space-y-3">
-        <div className="flex gap-2 items-baseline">
-          <span className="text-xl text-zinc-300 font-semibold">Dia 19</span>
-          <span className="text-xs text-zinc-500">Segunda-feira</span>
-        </div>
-        
-        <div className="flex items-center px-4 py-2.5 bg-zinc-900 shadow-shape rounded-xl justify-between">
-          <div className="flex gap-3 items-center">
-            <CircleCheck className="size-5 text-lime-300" />
-            <p className="text-zinc-100 text-md">Academia em grupo</p>
-          </div>
-          <div>
-            <span className="text-sm text-zinc-400">08:00h</span>
-          </div>
-        </div>
-
-        <div className="flex items-center px-4 py-2.5 bg-zinc-900 shadow-shape rounded-xl justify-between">
-          <div className="flex gap-3 items-center">
-            <CircleCheck className="size-5 text-lime-300" />
-            <p className="text-zinc-100 text-md">Almoço</p>
-          </div>
-          <div>
-            <span className="text-sm text-zinc-400">12:00h</span>
-          </div>
-        </div>
-
-        <div className="flex items-center px-4 py-2.5 bg-zinc-900 shadow-shape rounded-xl justify-between">
-          <div className="flex gap-3 items-center">
-            <CircleCheck className="size-5 text-lime-300" />
-            <p className="text-zinc-100 text-md">Gaming session</p>
-          </div>
-          <div>
-            <span className="text-sm text-zinc-400">14:00h</span>
-          </div>
-        </div>
-
-        <div className="flex items-center px-4 py-2.5 bg-zinc-900 shadow-shape rounded-xl justify-between">
-          <div className="flex gap-3 items-center">
-            <CircleCheck className="size-5 text-lime-300" />
-            <p className="text-zinc-100 text-md">Jantar</p>
-          </div>
-          <div>
-            <span className="text-sm text-zinc-400">21:00h</span>
-          </div>
-        </div>
-      </div>
-
-      <div className="space-y-3">
-        <div className="flex gap-2 items-baseline">
-          <span className="text-xl text-zinc-300 font-semibold">Dia 20</span>
-          <span className="text-xs text-zinc-500">Terça-feira</span>
-        </div>
-        <div className="flex items-center px-4 py-2.5 bg-zinc-900 shadow-shape rounded-xl justify-between">
-          <div className="flex gap-3 items-center">
-            <CircleCheck className="size-5 text-lime-300" />
-            <p className="text-zinc-100 text-md">Academia em grupo</p>
-          </div>
-          <div>
-            <span className="text-sm text-zinc-400">08:00h</span>
-          </div>
-        </div>
-
-        <div className="flex items-center px-4 py-2.5 bg-zinc-900 shadow-shape rounded-xl justify-between">
-          <div className="flex gap-3 items-center">
-            <CircleDashed className="size-5 text-zinc-400" />
-            <p className="text-zinc-100 text-md">Almoço</p>
-          </div>
-          <div>
-            <span className="text-sm text-zinc-400">12:00h</span>
-          </div>
-        </div>
-
-        <div className="flex items-center px-4 py-2.5 bg-zinc-900 shadow-shape rounded-xl justify-between">
-          <div className="flex gap-3 items-center">
-            <CircleDashed className="size-5 text-zinc-400" />
-            <p className="text-zinc-100 text-md">Jantar</p>
-          </div>
-          <div>
-            <span className="text-sm text-zinc-400">21:00h</span>
-          </div>
-        </div>
-      </div>
-
-      <div className="space-y-3">
-        <div className="flex gap-2 items-baseline">
-          <span className="text-xl text-zinc-300 font-semibold">Dia 21</span>
-          <span className="text-xs text-zinc-500">Quarta-feira</span>
-        </div>
-        <div className="flex items-center px-4 py-2.5 bg-zinc-900 shadow-shape rounded-xl justify-between">
-          <div className="flex gap-3 items-center">
-            <CircleDashed className="size-5 text-zinc-400" />
-            <p className="text-zinc-100 text-md">Churrasco</p>
-          </div>
-          <div>
-            <span className="text-sm text-zinc-400">21:00h</span>
-          </div>
-        </div>
-      </div>
-
-      <div className="space-y-3">
-        <div className="flex gap-2 items-baseline">
-          <span className="text-xl text-zinc-300 font-semibold">Dia 22</span>
-          <span className="text-xs text-zinc-500">Quinta-feira</span>
-        </div>
-        <div className="flex items-center px-4 py-2.5 bg-zinc-900 shadow-shape rounded-xl justify-between">
-          <div className="flex gap-3 items-center">
-            <CircleDashed className="size-5 text-zinc-400" />
-            <p className="text-zinc-100 text-md">Academia em grupo</p>
-          </div>
-          <div>
-            <span className="text-sm text-zinc-400">08:00h</span>
-          </div>
-        </div>
-
-        <div className="flex items-center px-4 py-2.5 bg-zinc-900 shadow-shape rounded-xl justify-between">
-          <div className="flex gap-3 items-center">
-            <CircleDashed className="size-5 text-zinc-400" />
-            <p className="text-zinc-100 text-md">Almoço</p>
-          </div>
-          <div>
-            <span className="text-sm text-zinc-400">12:00h</span>
-          </div>
-        </div>
-
-        <div className="flex items-center px-4 py-2.5 bg-zinc-900 shadow-shape rounded-xl justify-between">
-          <div className="flex gap-3 items-center">
-            <CircleDashed className="size-5 text-zinc-400" />
-            <p className="text-zinc-100 text-md">Jantar</p>
-          </div>
-          <div>
-            <span className="text-sm text-zinc-400">21:00h</span>
-          </div>
-        </div>              
-      </div>
-
-      <div className="space-y-3">
-        <div className="flex gap-2 items-baseline">
-          <span className="text-xl text-zinc-300 font-semibold">Dia 23</span>
-          <span className="text-xs text-zinc-500">Sexta-feira</span>
-        </div>
-        <p className="text-zinc-500 text-sm">Nenhuma atividade cadastrada nessa data.</p>
-      </div>
+        )
+      })}
     </div>
   )
 }
